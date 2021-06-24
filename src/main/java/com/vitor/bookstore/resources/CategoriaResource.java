46b592fc49b585686c4358baabb866107a9193f3
Vitor Hugo Kuhn Zary <vitorhbts@gmail.com>
2021-06-24 15:31:29 -0300

    Update para categoria

--- src/main/java/com/vitor/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/vitor/bookstore/resources/CategoriaResource.java
@@ -32,12 +32,18 @@ public class CategoriaResource {
         return ResponseEntity.ok().body(listDTO);
     }
 
-    @RequestMapping(method = RequestMethod.POST, consumes = "application/json")
     @PostMapping
     public ResponseEntity<Categoria> create(@RequestBody Categoria obj){
         obj = service.create(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<CategoriaDTO> update(@PathVariable Integer id, @RequestBody CategoriaDTO objDto){
+        Categoria newObj = service.update(id, objDto);
+        return ResponseEntity.ok().body(new CategoriaDTO(newObj));
+    }
+
 
 }
--- src/main/java/com/vitor/bookstore/services/CategoriaService.java
+++ src/main/java/com/vitor/bookstore/services/CategoriaService.java
@@ -1,6 +1,7 @@
 package com.vitor.bookstore.services;
 
 import com.vitor.bookstore.domain.Categoria;
+import com.vitor.bookstore.dtos.CategoriaDTO;
 import com.vitor.bookstore.repositories.CategoriaRepository;
 import com.vitor.bookstore.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +28,13 @@ public class CategoriaService {
 
     public Categoria create(Categoria obj) {
         obj.setId(null);
+        return repository.save(obj);
+    }
+
+    public Categoria update(Integer id, CategoriaDTO objDto) {
+        Categoria obj = findById(id);
+        obj.setNome(objDto.getNome());
+        obj.setDescricao(objDto.getDescricao());
         return repository.save(obj);
     }
 }
