85599a28f2763e7360ab04d44891115c35df0101
Vitor Hugo Kuhn Zary <vitorhbts@gmail.com>
2021-06-28 13:35:56 -0300

    Listando todos os livros por categoria

--- src/main/java/com/vitor/bookstore/repositories/LivroRepository.java
+++ src/main/java/com/vitor/bookstore/repositories/LivroRepository.java
@@ -2,9 +2,17 @@ package com.vitor.bookstore.repositories;
 
 import com.vitor.bookstore.domain.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LivroRepository extends JpaRepository<Livro, Integer>{
 
+    @Query(
+            value = "SELECT obj FROM Livro as obj WHERE obj.categoria.id = :id_cat ORDER BY obj.titulo"
+    )
+    List<Livro> findAllByCategoria(@Param(value = "id_cat") Integer id_cat);
 }
